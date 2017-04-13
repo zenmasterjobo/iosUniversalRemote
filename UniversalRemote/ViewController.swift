f3f1d4bb87a4c33b45ff676a69972778c3d6a646
Brian Crosser <briancrosser@Brians-MacBook-Pro.local>
2017-04-12 17:48:27 -0700

    initial commit

---
+++ UniversalRemote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UniversalRemote
+//
+//  Created by Brian Crosser on 4/12/17.
+//  Copyright © 2017 CCB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
